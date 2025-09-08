50a73f96f0368c122fba415b17306aa90a1662b6
Bashir Kabiri <bkabiri@me.com>
2025-09-08 22:56:48 +0100

    git

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,11 @@ rm -f /usr/src/app/tmp/pids/server.pid
 echo "bundle install..."
 bundle check || bundle install --jobs 4
 
+# Precompile assets (only in production)
+if [ "$RAILS_ENV" = "production" ]; then
+  echo "Precompiling assets..."
+  bundle exec rake assets:precompile
+fi
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
-
