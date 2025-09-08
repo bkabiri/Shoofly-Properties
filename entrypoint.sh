5e3c592637a77f339348d506abaeb27f464e1550
Bashir Kabiri <bkabiri@me.com>
2025-09-09 00:04:26 +0100

    push

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /usr/src/app/tmp/pids/server.pid
 
-echo "bundle check/install…"
+echo "bundle install..."
 bundle check || bundle install --jobs 4
 
-# No assets:precompile here – we precompiled during the image build.
-
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
+
