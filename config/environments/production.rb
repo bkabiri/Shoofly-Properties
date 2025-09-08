30c63e14ff93eb7b0e2c6b408df1f277fdbea440
Bashir Kabiri <bkabiri@me.com>
2025-09-08 21:48:49 +0100

    fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
 
   # Eager load code on boot.
   config.eager_load = true
-
+  config.require_master_key = false
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
