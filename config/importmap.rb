6d5e390649bd26f5ead1ac95d1c46ec0b0887a4d
Bashir Kabiri <bkabiri@me.com>
2025-09-10 17:49:20 +0100

    importma[

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,6 @@
-# Pin npm packages by running ./bin/importmap
-
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "application"
+pin "@hotwired/turbo-rails",       to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus",          to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading",  to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-
+pin "confirm_adapter", to: "confirm_adapter.js" # since you import it directly
