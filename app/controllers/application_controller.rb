a5f77ee4848e1f6f25e41b5a61100f7c10df7ebc
Bashir Kabiri <bkabiri@me.com>
2025-09-14 11:50:12 +0100

    after sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
   
   # Where to send users after sign up / sign in
-  def after_sign_up_path_for(resource)
-    root_path # or dashboard_path if you have one
-  end
 
-  def after_sign_in_path_for(resource)
-    root_path # or dashboard_path
-  end
   private
   def after_sign_in_path_for(resource)
     # Prefer stored location if Devise saved one (e.g., you were bounced)
