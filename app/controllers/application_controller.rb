3120a8908f3c11eafc2fb463e35c88dae6590e2b
Bashir Kabiri <bkabiri@me.com>
2025-09-14 12:59:40 +0100

    mail server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   
   # Where to send users after sign up / sign in
-
-  private
-  def after_sign_in_path_for(resource)
-    # Prefer stored location if Devise saved one (e.g., you were bounced)
-    stored_location_for(resource) ||
-      (Rails.env.development? ? listings_path : root_path)
-  end
 end
